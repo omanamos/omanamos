c0de7c9a9209196905aa7c06ecb02068270f78f8
omanamos <roman@promantek.com>
2010-10-24 15:09:33 -0700

    last work on laptop

--- default.js
+++ default.js
@@ -3,21 +3,32 @@ $(document).ready(loadPage);
 function loadPage(){
 	$('.menu_bar_link').hover(mouseOver, mouseOut);
 	$('.menu_bar_link').click(loadContent);
+	$('.default_value').formatter();
+	$('#contact_form_content').validate();
+	
+	loadContent('description_link');
 }
 
 function mouseOver(){
-	$(this).animate({backgroundColor: '#0E6B1F'}, {queue: false, duration: 500});
+	if(!$(this).hasClass('active')){
+		$(this).animate({backgroundColor: '#4FDE43'}, {queue: false, duration: 500});
+	}
 }
 
 function mouseOut(){
-	$(this).animate({backgroundColor: '#138A29'}, {queue: true, duration: 500});
+	if(!$(this).hasClass('active')){
+		$(this).animate({backgroundColor: '#2C6E16'}, {queue: true, duration: 500});
+	}
 }
 
 function loadContent(content_id){
-	var page = content_id
+	var page = content_id;
 	if(typeof page != 'string'){
 		page = this.id
 	}
+	
+	$('.active').removeClass('active');
+	$('#' + page).addClass('active');
 	$('.left_content').hide();
 	$('#' + page.split('_')[0] + '_content').fadeIn('slow');
 }
